bcda09c09c93657f519635c619ca38c3382ffc1d
jpamplona <pamplona.julius@gmail.com>
2020-10-04 10:48:23 +0800

    properly handle input file errors

--- src/file.js
+++ src/file.js
@@ -5,12 +5,21 @@ const read = (path) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (err, data) => {
       if (err) {
+        // check if file doesn't exist
         // eslint-disable-next-line no-param-reassign
-        err.message = `File System Error: ${err.message}`;
-        reject(err);
+        if (err.code === 'ENOENT') err.message = `Input file '${path}' not found.`;
+        return reject(err);
       }
-      resolve(JSON.parse(data));
+
+      // check if valid JSON
+      try {
+        return resolve(JSON.parse(data));
+      } catch (e) {
+        return reject(new Error('Invalid input file: Expecting JSON file.'));
+      }
     });
+  }).catch((err) => {
+    throw new Error(`Failed to read input file.\n${err.message}`);
   });
 };
 
